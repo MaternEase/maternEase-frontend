b5fa7884c7a1de49a5fbb9989fd1bee2a56abaee
Sadeep Chathushan <chathushan43sadeep@gmail.com>
2024-06-30 19:45:25 +0530

    Change login ui

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,16 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Button from '../Shared/Button';
-import Input from '../Shared/Input';
 import { useAuth } from '../../hooks/useAuth';
 import loginMom from '../../assets/images/loginMom.webp'; // Import the image
 
 const SignIn = () => {
   const { signIn } = useAuth();
   const [dimensions, setDimensions] = useState({
-    marginLeft: '14rem',
-    marginTop: '5rem',
-    marginRight: '14rem',
-    marginBottom: '5rem',
+    marginLeft: '1rem',
+    marginTop: '2rem',
+    marginRight: '1rem',
+    marginBottom: '2rem',
   });
 
   const handleResize = () => {
@@ -25,23 +23,23 @@ const SignIn = () => {
       });
     } else if (width <= 768) {
       setDimensions({
-        marginLeft: '5rem',
+        marginLeft: '2rem',
         marginTop: '3rem',
-        marginRight: '5rem',
+        marginRight: '2rem',
         marginBottom: '3rem',
       });
     } else if (width <= 1024) {
       setDimensions({
-        marginLeft: '10rem',
-        marginTop: '5rem',
-        marginRight: '10rem',
-        marginBottom: '5rem',
+        marginLeft: '3rem',
+        marginTop: '4rem',
+        marginRight: '3rem',
+        marginBottom: '4rem',
       });
     } else {
       setDimensions({
-        marginLeft: '14rem',
+        marginLeft: '5rem',
         marginTop: '5rem',
-        marginRight: '14rem',
+        marginRight: '5rem',
         marginBottom: '5rem',
       });
     }
@@ -55,57 +53,136 @@ const SignIn = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const { username, password } = event.target.elements;
-    signIn(username.value, password.value);
+    const { email, password } = event.target.elements;
+    signIn(email.value, password.value);
   };
 
   const { marginLeft, marginTop, marginRight, marginBottom } = dimensions;
 
   return (
     <div
-      className="flex items-center justify-center bg-center bg-cover responsive-background"
+      className="flex items-center justify-end bg-center bg-cover responsive-background"
       style={{
         backgroundImage: `url(${loginMom})`,
-        height: `calc(100vh - ${marginTop} - ${marginBottom})`,
-        width: `calc(100vw - ${marginLeft} - ${marginRight})`,
-        marginLeft,
-        marginTop,
-        marginRight,
-        marginBottom,
+        minHeight: '100vh',
+        paddingLeft: marginLeft,
+        paddingTop: marginTop,
+        paddingRight: marginRight,
+        paddingBottom: marginBottom,
         boxSizing: 'border-box',
-        border: '2px solid rgba(0, 0, 0, 0.1)',
-        borderRadius: '29px',
-        boxShadow: '0 15px 20px rgba(1, 1, 1, 0.7)',
       }}
     >
-      <div className="flex items-center justify-center w-full h-full p-4">
-        <form
-          className="flex flex-col items-center w-full max-w-md p-10 rounded-lg shadow-2xl bg-sky-100 bg-opacity-60 backdrop-blur-md responsive-form"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="mb-6 text-3xl font-bold text-gray-800">Sign In</h1>
-          <Input
-            type="text"
-            placeholder="Username"
-            name="username"
-            className="w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <Input
-            type="password"
-            placeholder="Password"
-            name="password"
-            className="w-full px-4 py-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <a href="#" className="mb-4 text-sm text-blue-500 hover:text-blue-700">
-            Forgot your password?
-          </a>
-          <Button
-            type="submit"
-            className="w-full py-2 font-bold text-white transition duration-300 bg-blue-500 rounded hover:bg-blue-700"
-          >
-            Sign In
-          </Button>
-        </form>
+      <div className="relative w-full max-w-md py-3 sm:max-w-xl">
+        <div className="absolute inset-0 transform -skew-y-6 shadow-lg bg-gradient-to-r from-cyan-400 to-sky-500 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div className="max-w-md mx-auto">
+            <div>
+              <h1 className="text-2xl font-semibold">Login</h1>
+            </div>
+            <div className="divide-y divide-gray-200">
+              <form onSubmit={handleSubmit}>
+                <div className="py-8 space-y-4 text-base leading-6 text-gray-700 sm:text-lg sm:leading-7">
+                  <div className="relative">
+                    <input
+                      autoComplete="off"
+                      id="email"
+                      name="email"
+                      type="text"
+                      className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:border-rose-600"
+                      placeholder="Email address"
+                    />
+                    <label
+                      htmlFor="email"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Email Address
+                    </label>
+                  </div>
+                  <div className="relative">
+                    <input
+                      autoComplete="off"
+                      id="password"
+                      name="password"
+                      type="password"
+                      className="w-full h-10 text-gray-900 placeholder-transparent border-b-2 border-gray-300 peer focus:outline-none focus:border-rose-600"
+                      placeholder="Password"
+                    />
+                    <label
+                      htmlFor="password"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                    >
+                      Password
+                    </label>
+                  </div>
+                  <div className="relative">
+                    <button
+                      type="submit"
+                      className="px-2 py-1 text-white rounded-md bg-cyan-500"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="flex justify-center w-full mt-6">
+              <button className="flex items-center px-6 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-300 rounded-lg shadow-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+                <svg
+                  className="w-6 h-6 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  width="800px"
+                  height="800px"
+                  viewBox="-0.5 0 48 48"
+                  version="1.1"
+                >
+                  <title>Google-color</title>
+                  <desc>Created with Sketch.</desc>
+                  <defs></defs>
+                  <g
+                    id="Icons"
+                    stroke="none"
+                    strokeWidth="1"
+                    fill="none"
+                    fillRule="evenodd"
+                  >
+                    <g
+                      id="Color-"
+                      transform="translate(-401.000000, -860.000000)"
+                    >
+                      <g
+                        id="Google"
+                        transform="translate(401.000000, 860.000000)"
+                      >
+                        <path
+                          d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                          id="Fill-1"
+                          fill="#FBBC05"
+                        ></path>
+                        <path
+                          d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                          id="Fill-2"
+                          fill="#EB4335"
+                        ></path>
+                        <path
+                          d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                          id="Fill-3"
+                          fill="#34A853"
+                        ></path>
+                        <path
+                          d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                          id="Fill-4"
+                          fill="#4285F4"
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+                <span>Continue with Google</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
