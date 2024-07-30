969d995f003a9172a1962cda058452ae68b26337
Amayaherath0121 <145965274+Amayaherath0121@users.noreply.github.com>
2024-07-30 18:05:38 +0530

    Create Profile

--- src/pages/ClinicAttendee/Growth.jsx
+++ src/pages/ClinicAttendee/Growth.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import { Box, Button } from '@mui/material';
-import { Link } from 'react-router-dom'; // Import Link component
+import { Link } from 'react-router-dom';
+import eye from "../../assets/images/eye.png";
+
+
+const imagePaths = [
+  '../../assets/images/eye.png',
+  '/path/to/image2.jpg',
+  '/path/to/image3.jpg',
+  '/path/to/image4.jpg',
+  '/path/to/image5.jpg',
+  '/path/to/image6.jpg',
+];
+
+const urls = [
+  '/child/Child_details1',
+  '/child/Child_details2',
+  '/child/Child_details3',
+  '/child/Child_details4',
+  '/child/Child_details5',
+  '/child/Child_details6',
+];
 
 const Growth = () => (
   <>
@@ -12,7 +32,7 @@ const Growth = () => (
     </div>
    
     <div className="grid grid-cols-3 gap-4 p-8 mx-auto my-8 bg-white rounded-lg shadow-lg" style={{ width: '90%' }}>
-      {[...Array(6)].map((_, index) => (
+      {imagePaths.map((path, index) => (
         <div
           key={index}
           className="container p-4 bg-white rounded-lg shadow-lg"
@@ -23,13 +43,13 @@ const Growth = () => (
             style={{ width: '100%', height: '200px', margin: '1px' }}
           >
             <img
-              src="" // Replace with your image path
-              alt={`Description of image ${index + 1}`} // Replace with a description of the image
+              src={eye}
+              alt="eye"
               style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '0.5rem' }}
             />
           </div>
           <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
-            <Link to="/page" style={{ textDecoration: 'none' }}>
+            <Link to={urls[index]} style={{ textDecoration: 'none' }}>
               <Button
                 variant="contained"
                 color="primary"
