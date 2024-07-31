885f2f02316dee8a5576a43ba6155a4c23b11818
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-07-31 09:59:51 +0530

    finalize the admin dashboard

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,25 +10,27 @@ const ReusableCard = ({ card, selectedCard, handleCardClick, menu }) => (
     onClick={() => handleCardClick(card.key)}
     hoverable
   >
-    <Row justify="space-between">
+    <Row justify="space-between" align="middle">
       <Col>
-        {card.icon}
-        <span className="card-title">{card.title}</span>
+        <div className="card-header">
+          {card.icon}
+          <span className="card-title">{card.title}</span>
+        </div>
       </Col>
-      <Col>
+      {/* <Col>
         <Dropdown overlay={menu} trigger={['click']}>
           <MoreHoriz className="more-icon" />
         </Dropdown>
-      </Col>
+      </Col> */}
     </Row>
     <Statistic
       value={card.value}
       valueStyle={{ color: card.color }}
-      suffix={<span className="suffix-text">for today</span>} 
+      suffix={<span className="suffix-text">for today</span>}
       style={{ marginTop: '16px' }}
     />
     <div className="card-paragraph">
-        <p>{card.paragraph}</p>
+      <p>{card.paragraph}</p>
     </div>
   </Card>
 );
