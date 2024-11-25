9bcad2b13b84546a0c243b357d4fdd6c346f40d0
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-11-25 16:26:12 +0530

    Create location file in mother and install react maps package

--- src/routes/ExpectantMotherRoutes.jsx
+++ src/routes/ExpectantMotherRoutes.jsx
@@ -17,6 +17,7 @@ import Profile from "../pages/ExpectantMother/Profile.jsx";
 import Allevents from "../pages/ExpectantMother/Allevents.jsx";
 import ChildrenDetails from "../pages/ExpectantMother/ChildrenDetails.jsx";
 import DeliveredMotherRecord from "../pages/ExpectantMother/DeliveredMotherRecord.jsx";
+import Location from "../pages/ExpectantMother/Location.jsx";
 // import WeightGainChart from "../pages/ExpectantMother/WeightGainChart.jsx";
 
 const ExpectantMotherRoutes = () => {
@@ -40,6 +41,7 @@ const ExpectantMotherRoutes = () => {
             <Route path="/childrendetails" element={<ChildrenDetails />} />
             <Route path="/children/child2" element={<Child2 />} />
             <Route path="/delivered" element={<DeliveredMotherRecord />} />
+            <Route path="/location" element={<Location />} />
             {/*<Route path="weightchart" element={<WeightGainChart />} />*/}
             {/*<Route path="mothers" element={<Mothers />} />*/}
             {/*<Route path="babies" element={<Babies />} />*/}
